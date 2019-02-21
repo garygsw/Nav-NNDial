a009d6a4bc5ccadcf5f403910e3fd47ce4aabf65
Gary Goh <garygsw@gmail.com>
2019-02-21 14:52:09 +0800

    update resource

--- loader/Kvret_DataReader.py
+++ loader/Kvret_DataReader.py
@@ -611,9 +611,8 @@ class DataReader(object):
         #             self.s2v['informable'][s].append(v.lower().strip())
         for entry in self.db:
             for s, v in entry.iteritems():
-                if s == 'name':
+                if s == 'name' and not v.startswith('home':
                     self.s2v['other']['name'].append(v.lower().strip())
-                    self.s2v['informable']['poi'].append(v.lower().strip())
 
         # sort values
         for s,vs in self.s2v['informable'].iteritems():
