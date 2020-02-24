fd9acd04cbd122118d63b5a99a4a0fd9368cc467
Gary Goh <garygsw@gmail.com>
2020-02-24 20:01:36 +0800

    change concat

--- nn/navnnsds.py
+++ nn/navnnsds.py
@@ -339,7 +339,7 @@ class NNSDS(BaseNNModule):
             # LSTM decoder
             if self.dec=='lstm' and self.learn_mode!='trk':
                 if self.ply == 'attention':
-                    input_belief_t = T.concatenate([belief_t, task_ref_t], axis=0)
+                    input_belief_t = T.concatenate([belief_t, task_ref_t], axis=1)
                 else:
                     input_belief_t = belief_t
                 prob_t, snapCost_t, prior_t, posterior_t, z_t, base_t, debugX = \
